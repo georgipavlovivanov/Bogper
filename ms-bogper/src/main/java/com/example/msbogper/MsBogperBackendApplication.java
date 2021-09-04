2d7489606e8b6ef6c898a64bc829276e7dfba82c
georgi ivanov <speedygonzales@abv.bg>
2021-09-04 12:38:38 +0300

    BOGPER-1 - backend project commit

---
+++ ms-bogper/src/main/java/com/example/msbogper/MsBogperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.msbogper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBogperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBogperBackendApplication.class, args);
+	}
+
+}
---
+++ ms-bogper/src/test/java/com/example/msbogper/MsBogperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msbogper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBogperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
